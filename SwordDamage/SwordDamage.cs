d522a44fbef0ae3557993f27bfab7642e48d6af5
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2025-11-03 14:28:00 +0500

    SwordDamage

---
+++ SwordDamage/SwordDamage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SwordDamage
+{
+    internal class SwordDamageCalculator
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        public int Damage { get; private set; }
+        private int roll;
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        private bool magic;
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        public void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+
+        public SwordDamageCalculator(int startingRoll)
+        {
+            Roll = startingRoll;
+            CalculateDamage();
+        }
+        
+    }
+}
