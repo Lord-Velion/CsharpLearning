d36576d1fc2dfc372a04ad41b5b416d648d1d65b
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-13 13:00:58 +0500

    Save current state of the project

---
+++ LINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
