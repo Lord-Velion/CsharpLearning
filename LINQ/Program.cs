6a49e6bd8253a014af681c9d6d30dcc9c1159198
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-13 13:27:55 +0500

    LINQ: exampleOne

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,7 +6,22 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            exampleOne();
+        }
+
+        static void exampleOne()
+        {
+            int[] scores = [97, 92, 81, 60];
+
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            foreach (var i in scoreQuery)
+            {
+                Console.WriteLine(i + " ");
+            }
         }
     }
 }
