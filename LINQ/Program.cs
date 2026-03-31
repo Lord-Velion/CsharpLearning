73b346cfaa8802a86bd17e59f1be36648ead0960
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-31 14:48:18 +0500

    LINQ: Filter data

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,133 +6,37 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            exampleFour();
+            filteringData_1();
         }
 
-        static void exampleOne()
+        static void filteringData_1()
         {
-            int[] scores = [97, 92, 81, 60];
+            string[] words = ["the", "quick", "brown", "fox", "jumps"];
 
-            IEnumerable<int> scoreQuery =
-                from score in scores
-                where score > 80
-                select score;
+            IEnumerable<string> query = from word in words
+                                        where word.Length == 3
+                                        select word;
 
-            foreach (var i in scoreQuery)
+            foreach (string str in query)
             {
-                Console.WriteLine(i + " ");
+                Console.WriteLine(str);
             }
         }
 
-        static void exampleTwo()
+        static void filteringData_2()
         {
-            // The Three Parts of a LINQ Query
-            // 1. Data source.
-            int[] numbers = [0, 1, 2, 3, 4, 5, 6];
+            string[] words = ["the", "quick", "brown", "fox", "jumps"];
 
-            // 2. Query creation
-            // numQuery is an IEnumerable<int>
-            var numQuery = from num in numbers
-                           where (num % 2) == 0
-                           select num;
+            IEnumerable<string> query =
+                words.Where(word => word.Length == 3);
 
-            // 3. Query execution
-            foreach (int num in numQuery)
+            foreach (string str in query)
             {
-                Console.WriteLine("{0,1} ", num);
+                Console.WriteLine(str);
             }
         }
 
-        static void exampleThree()
-        {
-            int[] numbers = [0, 1, 2, 3, 4, 5, 6];
-            var evenNumQuery = from num in numbers
-                               where (num % 2) == 0
-                               select num;
-
-            int evenNumCount = evenNumQuery.Count();
-
-            Console.WriteLine(evenNumCount);
-        }
-
-        static void exampleFour()
-        {
-            int[] numbers = [0, 1, 2, 3, 4, 5, 6];
-
-            List<int> numQuery2 = (from num in numbers
-                                   where (num % 2) == 0
-                                   select num).ToList();
-
-            var numQuery3 = (from num in numbers
-                             where (num % 2) == 0
-                             select num).ToArray();
-        }
-
-        static void exampleFive()
-        {
-            List<int> numbers = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-
-            IEnumerable<int> queryFactorsOfFour = from num in numbers
-                                                  where num % 4 == 0
-                                                  select num;
-
-            // Store the results in a new variable
-            // without executing a foreach loop.
-            var factorsofFourList = queryFactorsOfFour.ToList();
-
-            // Read and write from the newly created list to demonstrate that it holds data.
-            Console.WriteLine(factorsofFourList[2]);
-            factorsofFourList[2] = 0;
-            Console.WriteLine(factorsofFourList[2]);
-        }
-
-        static void exampleSix()
-        {
-            // Data source.
-            int[] scores = [90, 71, 82, 93, 75, 82];
-
-            // Query Expression.
-            IEnumerable<int> scoreQuery = //query variable
-                from score in scores //required
-                where score > 80 // optional
-                orderby score descending // optional
-                select score; //must end with select or group
-
-            // Execute the query to produce the results
-            foreach (var testScore in scoreQuery)
-            {
-                Console.WriteLine(testScore);
-            }
-
-            // Output: 93 90 82 82
-        }
-
-        static void exampleSeven()
-        {
-            int[] numbers = [5, 10, 8, 3, 6, 12];
-
-            //Query syntax:
-            IEnumerable<int> numQuery1 =
-                from num in numbers
-                where num % 2 == 0
-                orderby num
-                select num;
-
-            //Method syntax:
-            IEnumerable<int> numQuery2 = numbers
-                .Where(num => num % 2 == 0)
-                .OrderBy(n => n);
-
-            foreach (int i in numQuery1)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine(System.Environment.NewLine);
-            foreach (int i in numQuery2)
-            {
-                Console.Write(i + " ");
-            }
-        }
+              
     }
 }
 
